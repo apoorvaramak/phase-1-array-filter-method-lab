b77761ac859cddbbccf05e416da0e62963b5d257
Apoorva Ramakrishnan <apoorvar@comcast.net>
2021-06-29 10:01:03 -0500

    commit tree 740c7477c6e003a6707ea39da841ef5f16574453

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, name){
+    return arr.filter(function(str){ return(str.toUpperCase() === name.toUpperCase()); } ); 
+}
+function fuzzyMatch(arr, name){
+    return arr.filter(function(str){
+        return (str.slice(0, name.length) === name);
+    });
+}
+
+function matchName(arr, string){
+    return arr.filter(function(array){
+        return (array.name === string); 
+    })
+}
